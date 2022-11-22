c8c614a96c351642d58eb1f99cfba00832410bca
michimani <michimani210@gmail.com>
2022-11-23 00:48:49 +0900

    add test for eventbridge-scheduler-cfn

--- eventbridge-scheduler-cfn/eventbridge-scheduler-cfn_test.go
+++ eventbridge-scheduler-cfn/eventbridge-scheduler-cfn_test.go
@@ -1,26 +1,39 @@
 package main
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/aws/aws-cdk-go/awscdk/v2"
-// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
-// 	"github.com/aws/jsii-runtime-go"
-// )
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+	"github.com/aws/jsii-runtime-go"
+)
 
-// example tests. To run these tests, uncomment this file along with the
-// example resource in eventbridge-scheduler-cfn_test.go
-// func TestEventbridgeSchedulerCfnStack(t *testing.T) {
-// 	// GIVEN
-// 	app := awscdk.NewApp(nil)
+func TestEventBridgeSchedulerCfnStack(t *testing.T) {
+	// GIVEN
+	app := awscdk.NewApp(nil)
 
-// 	// WHEN
-// 	stack := NewEventbridgeSchedulerCfnStack(app, "MyStack", nil)
+	// WHEN
+	stack := NewEventBridgeSchedulerCfnStack(app, "MyStack", nil)
 
-// 	// THEN
-// 	template := assertions.Template_FromStack(stack)
+	// THEN
+	template := assertions.Template_FromStack(stack, &assertions.TemplateParsingOptions{})
 
-// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
-// 		"VisibilityTimeout": 300,
-// 	})
-// }
+	// Scheduler Schedule
+	template.HasResourceProperties(jsii.String("AWS::Scheduler::Schedule"), map[string]any{
+		"Description": jsii.String("EventBridge Scheduler for Lambda Function"),
+		"FlexibleTimeWindow": &map[string]any{
+			"Mode": jsii.String("OFF"), // "OFF"|"FLEXIBLE"
+		},
+		"ScheduleExpression": jsii.String("cron(* * */2 * ? *)"),
+	})
+
+	// Lambda Function
+	template.HasResourceProperties(jsii.String("AWS::Lambda::Function"), map[string]any{
+		"FunctionName": jsii.String("hello-aws-cdk-golang-scheduler-function"),
+		"Description":  jsii.String("Hello AWS CDK with Golang!"),
+		"Runtime":      jsii.String("go1.x"),
+		"Handler":      jsii.String("main"),
+		"MemorySize":   128,
+		"Timeout":      30,
+	})
+}
